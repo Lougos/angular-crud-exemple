dd5a22924f3e0d7ffb9ff30d275a06e87e79d486
Lilian Gautier <Lilian.gautier@devinci.Fr>
2019-09-20 08:31:56 +0200

    Step 17 mais problème tsé

--- angular-crud-exemple/src/app/products.service.ts
+++ angular-crud-exemple/src/app/products.service.ts
@@ -11,13 +11,44 @@ export class ProductsService {
   constructor(private http: HttpClient) { }
 
   addProduct(ProductName, ProductDescription, ProductPrice) {
+    console.log(ProductName, ProductDescription, ProductPrice);
     const obj = {
       ProductName,
       ProductDescription,
       ProductPrice
     };
-    console.log(obj);
     this.http.post(`${this.uri}/add`, obj)
         .subscribe(res => console.log('Done'));
   }
+
+  getProducts() {
+    return this
+           .http
+           .get(`${this.uri}`);
+  }
+
+  editProduct(id) {
+    return this
+            .http
+            .get(`${this.uri}/edit/${id}`);
+    }
+
+    deleteProduct(id) {
+      return this
+                .http
+                .get(`${this.uri}/delete/${id}`);
+    }
+
+    updateProduct(ProductName, ProductDescription, ProductPrice, id) {
+      const obj = {
+        ProductName,
+        ProductDescription,
+        ProductPrice
+      };
+      this
+        .http
+        .post(`${this.uri}/update/${id}`, obj)
+        .subscribe(res => console.log('Done'));
+  }
+
 }
